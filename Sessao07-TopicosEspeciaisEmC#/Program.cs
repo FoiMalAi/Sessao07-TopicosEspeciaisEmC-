f3bea6abe1f08d264e4ccbcf4b8c0ad979029ce6
Matheus de Lima <matheus.moreira127@gmail.com>
2025-01-15 09:57:34 -0300

    Add project files.

---
+++ Sessao07-TopicosEspeciaisEmC#/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Sessao07_TopicosEspeciaisEmC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+           //C# identifica qual a variavel pela informação
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+        }
+    }
+}
