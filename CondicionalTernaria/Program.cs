9a0da964903a8b6858c12902f6e17be94a617bf2
Matheus de Lima <matheus.moreira127@gmail.com>
2025-01-17 10:58:16 -0300

    Cond.Ternaria

---
+++ CondicionalTernaria/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CondicionalTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine());
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
