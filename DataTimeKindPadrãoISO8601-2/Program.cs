07da0f34a7ace46aba37bdf335e8c90f619f5026
Matheus de Lima <matheus.moreira127@gmail.com>
2025-03-18 15:36:36 -0300

    ISO8601

---
+++ DataTimeKindPadrãoISO8601-2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DataTimeKindPadrãoISO8601_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d1 = DateTime.Parse("2000-08-15 13:05:58");
+            DateTime d2 = DateTime.Parse("2000-08-15T13:05:58Z"); //iso8601
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+        }
+    }
+}
